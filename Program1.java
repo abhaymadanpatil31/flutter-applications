98a9feb3471e8490c2e8ffa11c0c8004ce7ecd27
abhaymadanpatil31 <flutterabhaypatil@gmail.com>
2024-01-03 18:10:16 +0530

    new

---
+++ Program1.java
@@ -0,0 +1,14 @@
+
+class Demo {
+
+	void printData(int n) {
+	
+		System.out.println("The Number is:"+n);
+	}
+
+	public static void main(String[] args) {
+	
+		Demo obj = new Demo();
+		obj.printData(10);
+	}
+}
